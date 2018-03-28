7493c731838dfa7df91c6d99a053066858b5ad90
lordid@rambler.ru <GitHub7>
2018-03-28 17:40:16 +0300

    Создан класс Options в пакете data, хранящий тестовые данные для параметризированных методов

---
+++ src/main/java/data/Options.java
@@ -0,0 +1,16 @@
+package data;
+
+public class Options {
+
+    public static String url = "https://mail.ru";
+
+    public static String titleName = "Mail.ru";
+    public static String loginFormName = "Имя ящика";
+    public static String passwordFormName = "Пароль";
+    public static String validLogin= "vlad.smirnov.new";
+    public static String validPassword = "v2s5m7c9";
+
+    public static String pageNameEmailAttribute = "data-title";
+    public static String pageNameEmail = "ПочтаMail.Ru";
+
+}
