a49ce3634bbf2c7ee1616746d6db793b495a21e2
lordid@rambler.ru <GitHub7>
2018-03-28 17:59:17 +0300

    Создал класс AuthenticationValidDataTestCase, выполняет тест-кейсы checkPageName, enterValidLoginPassword, pushButtonToComeIn, checkPageNameEmail, pushButtonExit

--- src/main/java/stepDefinition/AuthenticationValidDataTestCase.java
+++ src/main/java/stepDefinition/AuthenticationValidDataTestCase.java
@@ -1,4 +1,40 @@
 package stepDefinition;
 
-public class AuthenticationValidDataTestCase {
+import data.Data;
+import helper.InitialSteps;
+import org.testng.annotations.Test;
+
+
+public class AuthenticationValidDataTestCase extends InitialSteps {
+
+    @Test(dataProvider = "checkPageName",
+            dataProviderClass = Data.class)
+    public void checkPageName(String element) throws Throwable {
+        loginForm.checkElementPage(element);
+    }
+
+    @Test(dependsOnMethods = "checkPageName",
+            dataProvider = "enterValidLoginPassword",
+            dataProviderClass = Data.class)
+    public void enterValidLoginPassword(String el, String value) throws Throwable {
+        loginForm.enterValidLoginPassword(el, value);
+    }
+
+    @Test(dependsOnMethods = "enterValidLoginPassword")
+    public void pushButtonToComeIn() throws Throwable {
+        loginForm.pushButtonToComeIn();
+    }
+
+    @Test(dependsOnMethods = "pushButtonToComeIn")
+    public void checkPageNameEmail() throws Throwable {
+        basePageEmail.checkElementPage();
+    }
+
+    @Test(dependsOnMethods = "checkPageNameEmail")
+    public void pushButtonExit() throws Throwable {
+        basePageEmail.pushButtonExit();
+    }
+
+
+
 }
